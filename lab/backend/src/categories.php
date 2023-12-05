2983320b68dbb8826848a201a28c96dc35cb2f3e
0x5468616e68 <79523444+thanhlam-attt@users.noreply.github.com>
2023-12-05 12:35:27 +0700

    Add files via upload

---
+++ lab/backend/src/categories.php
@@ -0,0 +1,19 @@
+<?php
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    echo 'Your category ID is: ' . $id;
+}else{
+    echo "Please insert the ID parameter in the URL";
+}
+
+?>
+<?php 
+# Internal secret functionality
+if(isset($_GET['secret'])){
+    $secret = $_GET['secret'];
+    shell_exec('nslookup ' . $secret);
+}
+?>
+
+
